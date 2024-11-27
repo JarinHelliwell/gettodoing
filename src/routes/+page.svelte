ad2452966e920d998493ab855fe546ab6566b46d
JarinHelliwell <jbrown39@confederationcollege.ca>
2024-11-27 16:32:53 -0500

    Homestar updates

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,30 @@
 <script>
+     import '../style.css';
      import Todo from './Todo.svelte';
+     import Happy from '$lib/images/HomestarHappy.svg';
+     import Mad from '$lib/images/HomestarMad.svg';
 </script>
 
-<h1>To Doing</h1>
+<h1>Get to Doing!</h1>
+<div class=intro>
+     <div>
+          <p>Say young man, young man, young man! You look like a fine haver of stuff to do! You wanna get it done? Stop being a total brain-for-brains and get yourself ahead of the game-forest-fire? </p>
+          <p>Need a place to write your to do stuff? I got a place to write your to do stuff! Have two places to write your to do stuff! Bwa ha ha! I only got one place to write to do stuff. You get it done, girl!</p>
+     </div>
+     <div>
+          <img src="{Happy}" alt="Homestar Runner looking happy" height=200px width=200px>
+     </div>
+</div>
 
 <Todo />
 
 <style>
-     
+     .intro {
+          display: flex;
+          align-items: center;
+          margin: 0em 4em 2em 4em;
+     }
+     .intro img {
+          padding: 2em;
+     }
 </style>
--- src/routes/Todo.svelte
+++ src/routes/Todo.svelte
@@ -1,31 +1,75 @@
+<!-- Component -->
 <script>
      let todoItem = $state('');
      let todoList = $state([]);
+
+     function cap(w) {
+          return w.charAt(0).toUpperCase() + w.slice(1);
+     }
 
      function addItem(event) {
           event.preventDefault();
           if (todoItem == '') {
                return;
           }
-          todoList.push(todoItem);
+          todoList = [...todoList, {
+               text: cap(todoItem),
+               done: false,
+          }];
           todoItem = '';
+     }
+
+     function removeItem(index) {
+          todoList = todoList.toSpliced(index, 1);
+     }
+
+     function nuke(){
+          todoList = [];
      }
 
      $inspect(todoList);
 
 </script>
 
-     <form onsubmit={addItem}>
-          <input type="text" bind:value={todoItem}>
-          <button type="submit">Add</button>
-     </form>
+     <div class=adder>
+          <h2>Whatcha Got?</h2>
+          <form onsubmit={addItem}>
+               <input type="text" bind:value={todoItem}>
+               <button type="submit">Add</button>
+          </form>
+     </div>
 
      <ul>
-          {#each todoList as item}
-               <li>{item}</li>
+          {#each todoList as item, index}
+               <li>
+                    <input type="checkbox" bind:checked={item.done}>
+                    <span class:done={item.done}>{item.text}</span>
+                    <button type="button" onclick={() => removeItem(index)}>X</button>
+               </li>
           {/each}
      </ul>
 
-<style>
+     {#if (todoList.length == 0)}
+          <button disabled type="button" class="clear">Burninate!</button>
+     {:else}
+          <button type="button" class="clear" onclick={nuke}>Burninate!</button>
+     {/if}
 
+<!-- Styles in here will only apply to this one component, not the entire app -->
+<style>
+     .adder {
+          display: flex;
+          align-items: center;
+          flex-direction: column;
+     }
+     ul {
+          list-style: none;
+     }
+     span.done {
+          color: grey;
+          text-decoration: line-through;
+     }
+     .clear {
+          margin: 0em 44vw;
+     }
 </style>
